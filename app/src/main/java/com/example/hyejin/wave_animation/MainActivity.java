f7988d61791f9b139d67062214ebe9d04f112d73
Hyejin Kim <hyepago@naver.com>
2018-05-07 17:14:57 +0900

    gridlayout

--- app/src/main/java/com/example/hyejin/wave_animation/MainActivity.java
+++ app/src/main/java/com/example/hyejin/wave_animation/MainActivity.java
@@ -8,35 +8,47 @@ import me.itangqi.waveloadingview.WaveLoadingView;
 
 public class MainActivity extends AppCompatActivity {
 
-    WaveLoadingView waveLoadingView;
+    WaveLoadingView waveLoadingView[] = new WaveLoadingView[9];
     SeekBar seekBar;
+    Integer[] WaveIDs = {R.id.waveLoadingView1, R.id.waveLoadingView2, R.id.waveLoadingView3, R.id.waveLoadingView4, R.id.waveLoadingView5, R.id.waveLoadingView6, R.id.waveLoadingView7, R.id.waveLoadingView8, R.id.waveLoadingView9};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        waveLoadingView = (WaveLoadingView) findViewById(R.id.waveLoadingView);
+        for(int i=0; i<waveLoadingView.length; i++){
+            waveLoadingView[i] = (WaveLoadingView)findViewById(WaveIDs[i]);
+            waveLoadingView[i].setProgressValue(0);
+        }
+
         seekBar = (SeekBar) findViewById(R.id.seekBar);
-        waveLoadingView.setProgressValue(0);
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                waveLoadingView.setProgressValue(progress);
+                for(int i=0; i<waveLoadingView.length; i++){
+                    waveLoadingView[i].setProgressValue(progress);
+                }
 
                 if(progress < 50){
-                    waveLoadingView.setTopTitle("");
-                    waveLoadingView.setBottomTitle(String.format("%d%%", progress));
-                    waveLoadingView.setCenterTitle("");
-                } else if (progress == 80){
-                    waveLoadingView.setTopTitle("");
-                    waveLoadingView.setBottomTitle("");
-                    waveLoadingView.setCenterTitle(String.format("%d%%", progress));
+                    for(int i=0; i<waveLoadingView.length; i++){
+                        waveLoadingView[i].setTopTitle("");
+                        waveLoadingView[i].setBottomTitle(String.format("%d%%", progress));
+                        waveLoadingView[i].setCenterTitle("");
+                    }
+                } else if (progress == 50){
+                    for(int i=0; i<waveLoadingView.length; i++) {
+                        waveLoadingView[i].setTopTitle("");
+                        waveLoadingView[i].setBottomTitle("");
+                        waveLoadingView[i].setCenterTitle(String.format("%d%%", progress));
+                    }
                 } else {
-                    waveLoadingView.setTopTitle(String.format("%d%%", progress));
-                    waveLoadingView.setBottomTitle("");
-                    waveLoadingView.setCenterTitle("");
+                    for(int i=0; i<waveLoadingView.length; i++) {
+                        waveLoadingView[i].setTopTitle(String.format("%d%%", progress));
+                        waveLoadingView[i].setBottomTitle("");
+                        waveLoadingView[i].setCenterTitle("");
+                    }
                 }
             }
 
